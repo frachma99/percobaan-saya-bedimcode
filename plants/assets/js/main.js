b557e3a4448ede318843d3d60761921d86eb8efd
Febri Rachmawati <febri.rachma99@gmail.com>
2022-01-16 01:01:25 +0700

    menyelesaikan sampai menit 01.35.18
    nanti mulai dark/light theme

--- plants/assets/js/main.js
+++ plants/assets/js/main.js
@@ -1,20 +1,20 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
-// validate if constant exists 
-if(navToggle) {
-    navToggle.addEventListener('click', () => {
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
 /*===== MENU HIDDEN =====*/
-// validate if constant exist 
-if(navClose) {
-    navClose.addEventListener('click', () => {
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
@@ -22,60 +22,78 @@ if(navClose) {
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction() {
+function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // when we click on each nav__link, we remove the show-menu 
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader() {
+function scrollHeader(){
     const header = document.getElementById('header')
-    // when the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) {
-        header.classList.add('scroll-header')
-    } else {
-        header.classList.remove('scroll-header')
-    }
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', scrollHeader);
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== QUESTIONS ACCORDION ===============*/
-const accordionItems = document.querySelectorAll('.questions__item');
+const accordionItems = document.querySelectorAll('.questions__item')
 
-accordionItems.forEach((item) => {
-    const accordionHeader = item.querySelector('.questions__header');
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.questions__header')
 
-    accordionHeader.addEventListener('click', () => {
+    accordionHeader.addEventListener('click', () =>{
         const openItem = document.querySelector('.accordion-open')
 
-        toggleItem(item);
+        toggleItem(item)
 
-        if(openItem && openItem !== item) {
+        if(openItem && openItem!== item){
             toggleItem(openItem)
         }
     })
 })
 
-const toggleItem = (item) => {
-    const accordionContent = item.querySelector('.questions__content');
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.questions__content')
 
-    if(item.classList.contains('accordion-open')) {
+    if(item.classList.contains('accordion-open')){
         accordionContent.removeAttribute('style')
         item.classList.remove('accordion-open')
-    } else {
+    }else{
         accordionContent.style.height = accordionContent.scrollHeight + 'px'
-        item.classList.add('accordion-open');
+        item.classList.add('accordion-open')
     }
 
 }
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight ) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== DARK LIGHT THEME ===============*/ 
 
